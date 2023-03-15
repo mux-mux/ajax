4efb9a97fb07c8f33875311aeef145aaccb578b0
mikemouse <kmmyemail@ukr.net>
2023-03-15 13:48:18 +0200

    build: start point

---
+++ src/js/main.js
@@ -0,0 +1,184 @@
+window.addEventListener('DOMContentLoaded', () => {
+  //Tabs
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContainer = document.querySelector('.tabheader__items'),
+    tabContent = document.querySelectorAll('.tabcontent');
+
+  function hideTabContent() {
+    tabs.forEach((tab) => {
+      tab.classList.remove('tabheader__item_active');
+    });
+
+    tabContent.forEach((content) => {
+      content.classList.add('sidepanel__hide');
+      content.classList.remove('sidepanel__show');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabs[i].classList.add('tabheader__item_active');
+
+    tabContent[i].classList.remove('sidepanel__hide');
+    tabContent[i].classList.add('sidepanel__show');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsContainer.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((tab, i) => {
+        if (target == tab) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  //Timer
+  const deadline = '2023-03-12';
+
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((total / 1000 / 60) % 60),
+      seconds = Math.floor((total / 1000) % 60);
+
+    return {
+      total: total,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', deadline);
+
+  //Modal
+  const modalTrigger = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTrigger.addEventListener('click', () => {
+    modal.classList.add('sidepanel__show');
+    modal.classList.remove('sidepanel__hide');
+  });
+
+  modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('sidepanel__hide');
+    modal.classList.remove('sidepanel__show');
+  });
+
+  //Class for Cards
+
+  class itemCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.classes = classes;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.course = 27;
+      this.convertUSDUAH();
+    }
+
+    convertUSDUAH() {
+      this.price = this.price * this.course;
+    }
+
+    render() {
+      const div = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.div = 'menu__item';
+        div.classList.add(this.div);
+      } else {
+        this.classes.forEach((className) => div.classList.add(className));
+      }
+      div.innerHTML = `
+        <img src="${this.src}" alt="${this.alt}">
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      `;
+      this.parent.append(div);
+    }
+  }
+
+  new itemCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container',
+    'menu__item',
+    'big'
+  ).render();
+
+  new itemCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    '.menu .container'
+  ).render();
+
+  new itemCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    19,
+    '.menu .container',
+    'menu__item'
+  ).render();
+
+  // Forms
+  const forms = document.querySelectorAll('.form');
+
+  function postData(form) {
+    form.addEventListener('submit', () => {});
+  }
+});
